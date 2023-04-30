14deb56fabb6f552829c61c74ca7217abaf31b7a
matiasdantemar <matiasdantemar@hotmail.com>
2023-04-30 02:11:22 -0300

    modified urls

--- src/main/java/com/ejemplo/SpringBoot/Controller/CProyecto.java
+++ src/main/java/com/ejemplo/SpringBoot/Controller/CProyecto.java
@@ -22,7 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyecto")
-@CrossOrigin(origins = {"https://matias-portafolio.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://portafolio-matias.web.app","http://localhost:4200"})
 public class CProyecto {
     @Autowired
     SProyecto sProyecto;
